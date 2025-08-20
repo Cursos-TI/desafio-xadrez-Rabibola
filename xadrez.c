44f70420e8c314aebfa0c70761c9d6c0597f650d
Rabibola <hs931623@gmail.com>
2025-08-20 19:48:09 -0300

    criando movimentos da rainha com DO-WHILE

--- xadrez.c
+++ xadrez.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 
-
-int main() {
+int main()
+{
 
     printf("=== SIMULADOR DE MOVIMENTOS DE XADREZ ===\n\n");
-    
+
     // Torre: Move 5 casas para a direita usando FOR
     printf("Movimento da Torre (5 casas para a direita):\n");
     int casas_torre = 5;
-    
-    for(int i = 0; i < casas_torre; i++) {
+
+    for (int i = 0; i < casas_torre; i++)
+    {
         printf("Direita\n");
     }
     printf("\n");
@@ -18,13 +19,24 @@ int main() {
     printf("Movimento do Bispo (5 casas na diagonal cima/direita):\n");
     int casas_bispo = 5;
     int contador_bispo = 0;
-    
-    while(contador_bispo < casas_bispo) {
+
+    while (contador_bispo < casas_bispo)
+    {
         printf("Cima, Direita\n");
         contador_bispo++;
     }
     printf("\n");
-    
+
+    // Rainha: Move 8 casas para a esquerda usando DO-WHILE
+    printf("Movimento da Rainha (8 casas para a esquerda):\n");
+    int casas_rainha = 8;
+    int contador_rainha = 0;
+
+    do
+    {
+        printf("Esquerda\n");
+        contador_rainha++;
+    } while (contador_rainha < casas_rainha);
 
     return 0;
 }
